c6645aaa19d42e5693130e1b10223eb24b679f90
iStarzG <driftcd@mail.ru>
2024-03-10 03:53:20 +0500

    changed properties

--- src/test/java/config/DriverConfig.java
+++ src/test/java/config/DriverConfig.java
@@ -2,11 +2,10 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE)
+
 @Config.Sources({
         "system:properties",
-        "classpath:config/${driver}.properties"
-})
+        "classpath:config/${driver}.properties"})
 public interface DriverConfig extends Config {
 
     @Key("is.remote")
